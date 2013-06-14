25a54231971133842cf743a7c85b4c53bcf46c1f
Ganesh <gvenkatr@ee.oulu.fi>
2013-06-14 14:10:28 +0300

    updated the function plotFigure

--- plotFigure.m
+++ plotFigure.m
@@ -2,15 +2,46 @@
 function plotFigure(varargin)
 
 figColor = 'b';
-figMarker = 'p';
-figLineType = '-';
+figMarker = 'd';
+figLineType = '--';
 figLineWidth = 1;
 
-xValues = varargin{1,1};
-yValues = varargin{1,2};
-figPage = varargin{1,3};
-figType = varargin{1,4};
-
+switch nargin
+    case 1
+        xValues = varargin{1,1};
+        yValues = 1:length(xValues);
+        figPage = 1;figType = 'plot';
+        
+    case 2
+        xValues = varargin{1,1};
+        if ischar(varargin{1,2})
+            figType = varargin{1,2};
+            figPage = 1;
+            yValues = 1:length(xValues);
+        else
+            yValues = varargin{1,2};
+            figPage = 1;figType = 'plot';
+        end
+        
+    case 3
+        xValues = varargin{1,1};yValues = varargin{1,2};
+        if ischar(varargin{1,3})
+            figType = varargin{1,3};
+            figPage = 1;
+        else
+            figPage = varargin{1,3};
+            figType = 'plot';
+        end
+        
+    case 4
+        xValues = varargin{1,1};yValues = varargin{1,2};
+        figPage = varargin{1,3};figType = varargin{1,4};
+        
+    otherwise
+        display('invalid argument !');
+        
+end
+    
 figure(figPage);hold all;grid on;
 
 switch figType
